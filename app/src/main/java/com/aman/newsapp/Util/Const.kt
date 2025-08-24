2f38c92c0bcd8a1c2f80a96a0b01990897ecbaa9
aman1sr <Amrit@1234#>
2025-08-24 17:29:15 +0530

    add model and utils

---
+++ app/src/main/java/com/aman/newsapp/Util/Const.kt
@@ -0,0 +1,17 @@
+package com.aman.newsapp.Util
+
+
+object Const {
+
+    const val SEARCH_NEWS_TIME_DELAY = 500L
+    const val DEFAULT_QUERY_PAGE_SIZE = 20
+    const val DEFAULT_PAGE_NUM = 1
+    const val DEFAULT_COUNTRY = "in"
+    const val DEFAULT_LANGUAGE = "en"
+    const val DEFAULT_SOURCE = "abc-news"
+    //    const val API_KEY = BuildConfig.API_KEY       // todo: add API key
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val DB_NAME = "article_db"
+
+
+}
